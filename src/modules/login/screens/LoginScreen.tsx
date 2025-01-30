0399727c4d2a73b0e89e83eb8423ba738bd9f6cf
edgardleitte <edgarddasilvaleite@gmail.com>
2025-01-30 20:15:39 +0100

    feat: create module login image background

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import { BackgroundImage } from "../styles/LoginsScreen.styles";
+
+const LoginScreen = () => {
+    return (<div>
+        <BackgroundImage src="./background.png" />
+        </div>
+    );
+};
+
+export default LoginScreen;
