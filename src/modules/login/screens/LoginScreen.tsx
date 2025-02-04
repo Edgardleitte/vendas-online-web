8dcd752f11fa59de50262a486714d77a2b852997
edgardleitte <edgarddasilvaleite@gmail.com>
2025-02-04 20:18:05 +0100

    feat: insert state in login

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Button from "../../../shared/buttons/button/button";
 import Input from "../../../shared/inputs/input/input";
 import {
@@ -11,14 +13,30 @@ import {
 
 
 const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    alert(`username: ${username}, password: ${password}`);
+  };
+
+
   return (
     <ContainerLoginScreen>
       <ContainerLogin>
         <LimitedContainer>
           <LogoImage src="./logo.png" />
           <TitleLogin level={2} type="secondary">LOGIN</TitleLogin>
-          <Input title="Username:" />
-          <Input title="Password:" />
+          <Input title="Username:" margin="32px 0px 0px" onChange={handleUsername} value={username} />
+          <Input type="password" title="Password:" margin="32px 0px 0px" />
           <Button type="primary" margin="64px 0px 16px 0px">Enter</Button>
         </LimitedContainer>
       </ContainerLogin>
